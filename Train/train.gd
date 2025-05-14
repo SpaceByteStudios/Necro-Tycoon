6fd322d4c090b27b88d0a854e1a542b3e4b8f7d4
Hollow Cube <filipealves2006@icloud.com>
2025-05-14 15:50:47 +0200

    Updated Particles

--- Train/train.gd
+++ Train/train.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name Train
 
 @onready var engine = $Engine
+@onready var particles = $Engine/SmokeParticles
 @onready var wagon_scene = preload('res://Train/wagon.tscn')
 
 @export var rail_layer : RailManager
@@ -20,6 +21,8 @@ var traveled_dist : float
 var rail_length : float = 16.0
 
 func _ready() -> void:
+	particles.emitting = true
+	
 	match start_dir:
 		"Left":
 			direction = Vector2i.LEFT
